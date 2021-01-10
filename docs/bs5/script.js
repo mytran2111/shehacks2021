957931d9d1bf591400deb13d484e9e9e2ca8a916
ShereenElaidi <shereen.elaidi@mail.mcgill.ca>
2021-01-09 20:33:11 -0500

    first commit

---
+++ docs/bs5/script.js
@@ -0,0 +1,23 @@
+var choices = [];  // the array to store the  the books the user selects
+let paragraph; 
+
+function handleLoad() { 
+    paragraph = document.getElementById("selected-books"); 
+}
+
+
+$(document).ready(function() {
+    // var choices = []
+    // var picker = $('.selectpicker');
+    var picker = $('.js-example-basic-multiple').select2();
+    picker.change((event) => {
+        choices = picker.val();
+        console.log(choices)
+        // display the selected books
+        // format the string 
+        // let string = choices.toString(); 
+        // paragraph.textContent = string; 
+    });
+})
+
+window.addEventListener('load', handleLoad);  // when load event fires, execute handleLoad
