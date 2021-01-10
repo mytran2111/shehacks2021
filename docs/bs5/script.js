24592233ba99a00b438f2190b55cea6fc9f55ded
ShereenElaidi <shereen.elaidi@mail.mcgill.ca>
2021-01-10 00:38:44 -0500

    updates

--- docs/bs5/script.js
+++ docs/bs5/script.js
@@ -1,46 +1,25 @@
 var choices = [];  // the array to store the  the books the user selects
 let paragraph; 
+let count; 
 
 function handleLoad() { 
     paragraph = document.getElementById("selected-books"); 
-    loadBooks() 
-}
-
-function loadBooks() {
-    // load the data from the .json file 
-
-    const URL = "https://front-end-books.shereenelaidi.repl.co/books.json"; 
-
-    let dropdown = $('selector'); 
-    dropdown.empty()
-    dropdown.append('<option selected="true" disabled>Choose a Book</option>')
-    dropdown.prop('selectedIndex', 0)
-
-    //populate the dropdown with the list of books
-    console.log("loading books")
-    // $.getJSON(URL, function(data) {
-    //     $.each(data, function (key, entry) {
-    //         console.log("hi")
-    //         console.log(data)
-    //         dropdown.append($('<option></option>').attr('value', entry.authors).text(entry.title))
-    //     })
-    // }); 
+    count = document.getElementById("counter"); 
+    $(document).ready(function() {
+        var choices = []
+        // var picker = $('.selectpicker');
+        var picker = $('.selector').select2();
+        picker.change((event) => {
+            choices = picker.val();
+            console.log(choices)
+            let length = choices.length
+            count.textContent = `${length} / 10`
+            console.log(length) 
+        });
+    })
 }
 
 
-$(document).ready(function() {
-    // var choices = []
-    // var picker = $('.selectpicker');
-    var picker = $('.selector').select2();
-    picker.change((event) => {
-        choices = picker.val();
-        console.log(choices)
-        // display the selected books
-        // format the string 
-        // let string = choices.toString(); 
-        // paragraph.textContent = string; 
-    });
-})
 
 
 window.addEventListener('load', handleLoad);  // when load event fires, execute handleLoad
